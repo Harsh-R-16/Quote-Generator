8f63adee3849651a5230a53ba827dec4898bc83a
Harsh Gajera <harsh.gajera17@gmail.com>
2022-04-20 10:53:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React from "react";
+import { quotes } from "./data";
 function App() {
+  let [quote, setQuote] = React.useState({
+    text: "A good plan today is better than a perfect plan tomorrow.",
+    author: null,
+  });
+
+  const tweetQuote = () => {
+    const url = `https://twitter.com/intent/tweet?text=${quote.text} - ${quote.author}`;
+    window.open(url, "_blank");
+  };
+
+  const fetchQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((response) => {
+        setQuote(response[Math.floor(Math.random() * response.length)]);
+      })
+      .catch((err) => {
+        console.log(err);
+        setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+      });
+  };
+  // console.log(quote.text);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1 className={quote.text.length > 70 ? "long-quote" : ""}>
+        <i className="fa-solid fa-quote-left"></i>
+        {quote.text}
+      </h1>
+      <h2>- {quote.author ? quote.author : "Unknown"}</h2>
+      <div id="btns">
+        <button id="tweet" title="Tweet This!" onClick={tweetQuote}>
+          <i className="fa-brands fa-twitter"></i>
+        </button>
+        <button onClick={fetchQuote}>New Quote</button>
+      </div>
+    </main>
   );
 }
 
